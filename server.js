78ab0d5b66076ca01deafd38aef773191371f629
Sites Trends <sitesfortrends@gmail.com>
2026-04-04 22:35:21 -0400

    fix cors properly

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://sitesfortrends.com",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
