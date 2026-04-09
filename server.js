5655b93964f9a403a9315421b45cf6ef1c46085a
Sites Trends <sitesfortrends@gmail.com>
2026-04-09 11:38:43 -0400

    db ud 2

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ io.on("connection", (socket) => {
 
     const targetSocketId = users[targetId];
 
-    fetch("C:/assets/realtime/save_message.php", {   //  <<<< Added
+    fetch("C:xampp/htdocs/realtime/assets/save_message.php", {   //  <<<< Added
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(msg)
