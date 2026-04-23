ed394a00ab80a017a121edc4473238eac2c343a9
Sites Trends <sitesfortrends@gmail.com>
2026-04-23 13:24:38 -0400

    db

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const ADMIN_ID = "1";
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "",
   database: "sites"
