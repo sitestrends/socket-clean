efa189fc126d8476f3a2e0f901693c778fab0d7d
Sites Trends <sitesfortrends@gmail.com>
2026-04-24 00:38:39 -0400

    cors

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ io.on("connection", (socket) => {
   console.log("CONNECTED:", socket.id);
   console.log("SOCKET CONNECTED");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log("🚀 SERVER RUNNING ON PORT", PORT);
