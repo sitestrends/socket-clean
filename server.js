ca1fa623436f830b7e9b9cc3a99394bded1e9dd0
Sites Trends <sitesfortrends@gmail.com>
2026-04-24 00:27:45 -0400

    port

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ io.on("connection", (socket) => {
   console.log("CONNECTED:", socket.id);
   console.log("SOCKET CONNECTED");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
   console.log("🚀 SERVER RUNNING ON PORT", PORT);
