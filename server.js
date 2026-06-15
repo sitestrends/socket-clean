ae691bb2963b7ce8f2870bc995279472af901920
Sites Trends <sitesfortrends@gmail.com>
2026-06-15 04:38:30 -0400

    socket removed"

--- server.js
+++ server.js
@@ -31,18 +31,6 @@ io.on("connection", (socket) => {
 });
 
 socket.on("messages_seen", (data) => {
-
-  console.log("SEEN EVENT:", data);
-
-  setTimeout(() => {
-
-    loadChatMessages();
-
-  }, 300);
-
-});
-
-/*socket.on("messages_seen", (data) => {
   console.log("MESSAGES SEEN:", data);
 
   io.emit("messages_seen", {
@@ -50,7 +38,7 @@ socket.on("messages_seen", (data) => {
     chatUser: data.chatUser,
     affected: data.affected
   });
-}); */
+}); 
 
 /*  socket.on("typing", (data) => {
 console.log("TYPING EVENT:", data);
